f84342e40d4a551d13a9b69cf1922e062b8fa9e1
Fahim Mohammed <mohammedf2606@gmail.com>
2021-04-06 12:28:56 +0100

    Fix issue with webpacker

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
-
   get 'home/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   get '/users/auth/github/callback', to: 'home#callback'
-
   resources :repos do
     resources :files
   end
